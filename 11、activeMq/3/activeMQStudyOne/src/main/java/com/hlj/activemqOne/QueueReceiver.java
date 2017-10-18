3d5894f541c6ee55e48f5c16f43c0a6a4179a4e2
HealerJean123 <mxzdhealer@gmail.com>
2017-10-18 22:35:50 +0800

    activemq spring

---
+++ 11、activeMq/3/activeMQStudyOne/src/main/java/com/hlj/activemqOne/QueueReceiver.java
@@ -0,0 +1,68 @@
+package com.hlj.activemqOne;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.MapMessage;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQMessageConsumer;
+import org.apache.activemq.RedeliveryPolicy;
+
+public class QueueReceiver {
+	public static void main(String[] args) throws Exception {
+		ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(
+//				"tcp://localhost:61616");
+				"tcp://localhost:61616");
+		
+		RedeliveryPolicy policy = new RedeliveryPolicy();
+		policy.setMaximumRedeliveries(3);
+		cf.setRedeliveryPolicy(policy);
+		
+		Connection connection = cf.createConnection();
+		connection.start();
+		
+//		Enumeration names = connection.getMetaData().getJMSXPropertyNames();
+//		while(names.hasMoreElements()){
+//			String name = (String)names.nextElement();
+//			System.out.println("jmsx name==="+name);
+//		}
+		
+
+		final Session session = connection.createSession(Boolean.FALSE,
+				Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue("my-queue3");
+
+		ActiveMQMessageConsumer consumer = (ActiveMQMessageConsumer)session.createConsumer(destination);
+		
+		
+		int i = 0;
+		while (i < 3) {
+//			i++;
+//			TextMessage message = (TextMessage) consumer.receive();
+			
+			
+			
+			MapMessage message = (MapMessage) consumer.receive();
+			
+			
+			
+			
+//			session.commit();
+			
+//			if(i==2){
+//				message.acknowledge();
+//			}
+//			
+			System.out.println("收到消 息：" + message.getString("messageAA--" + i) 
+					+" , property=="+message.getStringProperty("extra"));
+			
+			i++;
+			
+//			session.commit();
+		}
+		session.close();
+		connection.close();
+	}
+
+}
