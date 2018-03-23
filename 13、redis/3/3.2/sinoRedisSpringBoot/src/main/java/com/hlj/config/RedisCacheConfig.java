97b546439656f91ac5987daa74478fc76b15f365
zhangyj123 <yujin.zhang@duodian.com>
2018-03-23 18:48:32 +0800

    redis

--- 13、redis/3/3.2/sinoRedisSpringBoot/src/main/java/com/hlj/config/RedisCacheConfig.java
+++ 13、redis/3/3.2/sinoRedisSpringBoot/src/main/java/com/hlj/config/RedisCacheConfig.java
@@ -2,6 +2,11 @@ package com.hlj.config;
 
 import java.lang.reflect.Method;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hlj.config.serializer.CustomJSONStringRedisSerializer;
+import com.hlj.config.serializer.CustomStringRedisSerializer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -11,6 +16,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -57,11 +64,21 @@ public class RedisCacheConfig extends  CachingConfigurerSupport{
 		RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
 		redisTemplate.setConnectionFactory(factory);
 
-		//key序列化方式;（不然会出现乱码;）,但是如果方法上有Long等非String类型的话，会报类型转换错误；
+		//key序列化方式,StringXX是转为String，JacksonXX是将对象转为json。
+		// 需要注意这里Key使用了StringRedisSerializer，那么Key只能是String类型的，不能为Long，Integer，否则会报错抛异常。
+		// 就是假如PostRepository里定义的@Cacheable（key="0"）的话就会报错，因为这样作为key的是int型，key必须为String。
 		//所以在没有自己定义key生成策略的时候，可以不配置,我就没有配置
 		RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long类型不可以会出现异常信息;
 		redisTemplate.setKeySerializer(redisSerializer);
-		redisTemplate.setHashKeySerializer(redisSerializer);
+
+		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+		ObjectMapper om = new ObjectMapper();
+		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		jackson2JsonRedisSerializer.setObjectMapper(om);
+
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
 
 		return redisTemplate;
 	}
